ec612fb74827b4b298b898471c0ccf3c7a98edd0
Rory Hackney <roryhackney@gmail.com>
2025-03-11 13:51:08 -0700

    StyleSheet object

--- components/TextInputWithLabel.tsx
+++ components/TextInputWithLabel.tsx
@@ -1,31 +1,34 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function TextInputWithLabel(props: {label: string}) {
-    const inputStyles = {
-        width: 272,
-        height: 44,
-        backgroundColor: "white",
-        borderWidth: 1,
-        borderRadius: 8,
-        borderColor: "black",
-        paddingLeft: 16, paddingRight: 16, paddingTop: 12, paddingBottom: 12
-    };
-    const labelStyle = {
-        lineHeight: 16,
-        paddingBottom: 5,
-        fontSize: 16,
-        fontWeight: 500 //medium
-    }
-    const containerStyles = {
-        marginBottom: 20
-    }
+    const styles = StyleSheet.create({
+        input: {
+            width: 272,
+            height: 44,
+            backgroundColor: "white",
+            borderWidth: 1,
+            borderRadius: 8,
+            borderColor: "black",
+            paddingLeft: 16, paddingRight: 16, paddingTop: 12, paddingBottom: 12
+        },
+        label: {
+            lineHeight: 16,
+            paddingBottom: 5,
+            fontSize: 16,
+            fontWeight: 500
+        },
+        container: {
+            marginBottom: 20
+        }
+    })
+
     return (
-        <View style={containerStyles}>
+        <View style={styles.container}>
             <Text 
                         nativeID={props.label}
-                        style={labelStyle}
+                        style={styles.label}
             >{props.label}</Text>
-            <TextInput  style={inputStyles}
+            <TextInput  style={styles.input}
                         accessibilityLabel={props.label}
                         accessibilityLabelledBy={props.label}
                         placeholder={props.label}
