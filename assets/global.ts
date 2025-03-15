3ab2194244ddbe4ebae842cde3c539305ccaa5e7
nguyen giang <giangthuynguyen2000@gmail.com>
2025-03-15 11:52:11 -0700

    Added Global ts

---
+++ assets/global.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff", 
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain", 
+  },
+});
