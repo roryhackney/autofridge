75a503429a1cd7ea53540cc523da1a58582b8c93
Rory Hackney <roryhackney@gmail.com>
2025-03-22 15:10:40 -0700

    working on modal popup

--- components/EditFoodInfo.tsx
+++ components/EditFoodInfo.tsx
@@ -1,19 +1,28 @@
-import {Modal} from "react-native";
+import {Modal, View} from "react-native";
 import TextInputWithLabel from "./TextInputWithLabel";
 import GenericButton from "./GenericButton";
 
-export default function EditFoodInfo(props: {visible: boolean, setIsVisible: Function, food: {name: string, category: string, description: string}}) {
-    return <Modal visible={props.visible} onRequestClose={() => props.setIsVisible(false)}>
-        <TextInputWithLabel label="Name" initialValue={props.food.name}/>
-        {/* TODO: make this a select input or RN equivalent?? */}
-        <TextInputWithLabel label="Category" initialValue={props.food.category}/>
-        {/* TODO: make this a textarea or RN equivalent?? */}
-        <TextInputWithLabel label="Description" initialValue={props.food.description}/>
-        <GenericButton isDark={true} title="Save changes" action={
-            () => {
-                console.log("Implement updating food info");
-                props.setIsVisible(false);
-            }
-        }/>
+export default function EditFoodInfo(props: {visible: boolean, setVisible: Function, food: {name: string, category: string, description: string}}) {
+    const style = {
+        width: 600,
+        height: 600,
+        backgroundColor: "white",
+        borderColor: "red",
+        borderWidth: 2
+    }
+    return <Modal transparent={true} visible={props.visible} onRequestClose={() => props.setVisible(false)}>
+        <View style={style}>
+            <TextInputWithLabel label="Name" initialValue={props.food.name}/>
+            {/* TODO: make this a select input or RN equivalent?? */}
+            <TextInputWithLabel label="Category" initialValue={props.food.category}/>
+            {/* TODO: make this a textarea or RN equivalent?? */}
+            <TextInputWithLabel label="Description" initialValue={props.food.description}/>
+            <GenericButton isDark={true} title="Save changes" action={
+                () => {
+                    console.log("Implement updating food info");
+                    props.setVisible(false);
+                }
+            }/>
+        </View>
     </Modal>
 }
