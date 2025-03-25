5dc2207421fe37e65e315be338e8bb748278a6f5
nguyen giang <giangthuynguyen2000@gmail.com>
2025-03-24 19:06:49 -0700

    Fixed Strange Symbols in Index

--- app/(logged-out)/index.tsx
+++ app/(logged-out)/index.tsx
@@ -1,7 +1,6 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
 import globalStyles from "@/assets/global";
-<<<<<<< HEAD
 import LogoLogin from "@/components/LogoLogin";
 
 export default function Index() {
@@ -18,16 +17,11 @@ export default function Index() {
           <Text style={globalStyles.genericButtonText}>Sign Up</Text>
         </View>
       </Link>
+      <Link style={globalStyles.links} href="/home">
+        <View style={globalStyles.genericButton}>
+          <Text style={globalStyles.genericButtonText}>Go to Home</Text>
+        </View>
+      </Link>
     </View>
   );
-=======
-
-export default function Index() {
-    return (
-        <View>
-            <Text>Implement Sign Up screen</Text>
-            <Link style={globalStyles.links} href="/home">Home</Link>
-        </View>
-    );
->>>>>>> bba3f2740d45e1d7002df31599a9e401e1af0f3f
 }
